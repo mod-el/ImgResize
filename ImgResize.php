871ad14d9a75664b20e3b0023f2e085665ff2365
Domenico Giambra <sviluppo@editt.it>
2018-01-20 22:25:40 +0100

    Type hints and code reformatting

--- ImgResize.php
+++ ImgResize.php
@@ -1,6 +1,7 @@
 <?php namespace Model\ImgResize;
 
-class ImgResize{
+class ImgResize
+{
 	/** @var resource */
 	protected $img;
 	/** @var int */
@@ -19,8 +20,9 @@ class ImgResize{
 	 * @param bool $ignore_non_existent
 	 * @throws \Exception
 	 */
-	public function __construct($url, $ignore_non_existent = false){
-		if(!file_exists($url) and !$ignore_non_existent){
+	public function __construct(string $url, bool $ignore_non_existent = false)
+	{
+		if (!file_exists($url) and !$ignore_non_existent) {
 			throw new \Exception('Non existing image');
 		}
 
@@ -28,7 +30,7 @@ class ImgResize{
 		$this->mime = $size['mime'];
 		$this->exif = @exif_read_data($url);
 
-		switch($this->mime){
+		switch ($this->mime) {
 			case 'image/jpeg':
 				$this->img = imagecreatefromjpeg($url);
 				break;
@@ -43,13 +45,13 @@ class ImgResize{
 				break;
 		}
 
-		if(!$this->img){
+		if (!$this->img) {
 			throw new \Exception('Image file not valid');
 		}
 
 		$ort = isset($this->exif['IFD0']['Orientation']) ? $this->exif['IFD0']['Orientation'] : (isset($this->exif['Orientation']) ? $this->exif['Orientation'] : 0);
 
-		switch($ort){
+		switch ($ort) {
 			case 3: // 180 rotate
 				$this->img = imagerotate($this->img, 180, 0);
 				break;
@@ -68,22 +70,25 @@ class ImgResize{
 	/**
 	 * @return bool
 	 */
-	public function isValid(){
-		return (bool) (is_resource($this->img) and get_resource_type($this->img)=='gd');
+	public function isValid(): bool
+	{
+		return (bool)(is_resource($this->img) and get_resource_type($this->img) == 'gd');
 	}
 
 	/**
 	 *
 	 */
-	public function __destruct(){
+	public function __destruct()
+	{
 		$this->destroy();
 	}
 
 	/**
 	 *
 	 */
-	public function destroy(){
-		if($this->isValid())
+	public function destroy()
+	{
+		if ($this->isValid())
 			imagedestroy($this->img);
 		$this->img = null;
 	}
@@ -92,43 +97,49 @@ class ImgResize{
 	 * @param array $newSizes
 	 * @return resource
 	 */
-	public function get(array $newSizes = []){
-		if(isset($newSizes['w']) and !isset($newSizes['h']))
-			$newSizes['h'] = $newSizes['w']*$this->h/$this->w;
-		if(isset($newSizes['h']) and !isset($newSizes['w']))
-			$newSizes['w'] = $newSizes['h']*$this->w/$this->h;
+	public function get(array $newSizes = [])
+	{
+		if (isset($newSizes['w']) and !isset($newSizes['h']))
+			$newSizes['h'] = $newSizes['w'] * $this->h / $this->w;
+		if (isset($newSizes['h']) and !isset($newSizes['w']))
+			$newSizes['w'] = $newSizes['h'] * $this->w / $this->h;
 
-		if(isset($newSizes['w'], $newSizes['h'])){
-			$ww = $newSizes['w']; $hh = $newSizes['h'];
-			$w = $this->w; $h = $this->h;
+		if (isset($newSizes['w'], $newSizes['h'])) {
+			$ww = $newSizes['w'];
+			$hh = $newSizes['h'];
+			$w = $this->w;
+			$h = $this->h;
 
-			if(!isset($newSizes['extend']))
+			if (!isset($newSizes['extend']))
 				$newSizes['extend'] = true;
 
-			$ratio = $w/$h; $rightRatio = $ww/$hh;
+			$ratio = $w / $h;
+			$rightRatio = $ww / $hh;
 
 			$newImg = imagecreatetruecolor($ww, $hh);
-			imagealphablending($newImg, false); ImageSaveAlpha($newImg, true);
-			ImageFill($newImg, 0, 0, IMG_COLOR_TRANSPARENT); imagealphablending($newImg, true);
+			imagealphablending($newImg, false);
+			ImageSaveAlpha($newImg, true);
+			ImageFill($newImg, 0, 0, IMG_COLOR_TRANSPARENT);
+			imagealphablending($newImg, true);
 
-			if(!$newSizes['extend']){
-				if($ratio<$rightRatio){
-					$new_width = $w*$hh/$h;
-					imagecopyresampled($newImg, $this->img, round(($ww-$new_width)/2), 0, 0, 0, $new_width, $hh, $w, $h);
-				}else{
-					$new_height = $h*$ww/$w;
-					imagecopyresampled($newImg, $this->img, 0, round(($hh-$new_height)/2), 0, 0, $ww, $new_height, $w, $h);
+			if (!$newSizes['extend']) {
+				if ($ratio < $rightRatio) {
+					$new_width = $w * $hh / $h;
+					imagecopyresampled($newImg, $this->img, round(($ww - $new_width) / 2), 0, 0, 0, $new_width, $hh, $w, $h);
+				} else {
+					$new_height = $h * $ww / $w;
+					imagecopyresampled($newImg, $this->img, 0, round(($hh - $new_height) / 2), 0, 0, $ww, $new_height, $w, $h);
 				}
-			}else{
-				if($ratio<$rightRatio){
-					$new_height = $h*$ww/$w;
-					imagecopyresampled($newImg, $this->img, 0, round(($hh-$new_height)/2), 0, 0, $ww, $new_height, $w, $h);
-				}else{
-					$new_width = $w*$hh/$h;
-					imagecopyresampled($newImg, $this->img, round(($ww-$new_width)/2), 0, 0, 0, $new_width, $hh, $w, $h);
+			} else {
+				if ($ratio < $rightRatio) {
+					$new_height = $h * $ww / $w;
+					imagecopyresampled($newImg, $this->img, 0, round(($hh - $new_height) / 2), 0, 0, $ww, $new_height, $w, $h);
+				} else {
+					$new_width = $w * $hh / $h;
+					imagecopyresampled($newImg, $this->img, round(($ww - $new_width) / 2), 0, 0, 0, $new_width, $hh, $w, $h);
 				}
 			}
-		}else{
+		} else {
 			$newImg = $this->getClone();
 			imagealphablending($newImg, false);
 			ImageSaveAlpha($newImg, true);
@@ -144,17 +155,18 @@ class ImgResize{
 	 * @return bool
 	 * @throws \Exception
 	 */
-	public function save($url, array $newSizes = []){
+	public function save(string $url, array $newSizes = []): bool
+	{
 		$newImg = $this->get($newSizes);
 
 		$mime = isset($newSizes['type']) ? $newSizes['type'] : $this->mime;
 
-		if(file_exists($url))
+		if (file_exists($url))
 			unlink($url);
 
 		$s = false;
 
-		switch($mime){
+		switch ($mime) {
 			case 'image/jpeg':
 				$s = imagejpeg($newImg, $url);
 				break;
@@ -178,7 +190,8 @@ class ImgResize{
 	/**
 	 * @return resource
 	 */
-	public function getClone() {
+	public function getClone()
+	{
 		//Get sizes from image.
 		$w = $this->w;
 		$h = $this->h;
@@ -190,11 +203,11 @@ class ImgResize{
 			$clone = imagecreatetruecolor($w, $h);
 			imagealphablending($clone, false);
 			imagesavealpha($clone, true);
-		}else{
+		} else {
 			$clone = imagecreate($w, $h);
 
 			//If the image has transparency...
-			if($trans >= 0) {
+			if ($trans >= 0) {
 				$rgb = imagecolorsforindex($this->img, $trans);
 				imagesavealpha($clone, true);
 				$trans_index = imagecolorallocatealpha($clone, $rgb['red'], $rgb['green'], $rgb['blue'], $rgb['alpha']);
@@ -212,18 +225,19 @@ class ImgResize{
 	 * @param string $file
 	 * @return bool
 	 */
-	public function applyWatermark($file){
-		if(!file_exists($file))
+	public function applyWatermark(string $file)
+	{
+		if (!file_exists($file))
 			return false;
 
 		$size = getimagesize($file);
-		$w = $this->w/4;
-		if($w>$size[0])
+		$w = $this->w / 4;
+		if ($w > $size[0])
 			$w = $size[0];
-		$h = $w*$size[1]/$size[0];
+		$h = $w * $size[1] / $size[0];
 
 		$x = 10;
-		$y = $this->h-$h-10;
+		$y = $this->h - $h - 10;
 
 		$wm = imagecreatefrompng($file);
 		return imagecopyresampled($this->img, $wm, $x, $y, 0, 0, $w, $h, $size[0], $size[1]);
